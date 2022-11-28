4b527aa1e2992a8e75050f6ac9de970bc56ed144
gitakileus <akileus0902@gmail.com>
2022-11-28 21:17:36 +0800

    update footer text style and add link to contact-us button

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -4,7 +4,8 @@ const Footer = () => {
 	return (
 		<footer>
 			<p className="contact-us">
-				Want to customize this game for your project? <u>Contact Us</u>
+				Want to customize this game for your project?&nbsp;
+				<a href="https://forms.gle/r1x2L8uTyEZqqgrb7">Contact Us</a>
 			</p>
 		</footer>
 	)
