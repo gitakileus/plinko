960b726efbeaa8ea71bfaad7439eca22b4aa8361
gitakileus <akileus0902@gmail.com>
2022-11-28 21:25:46 +0800

    update footer text

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<footer>
 			<p className="contact-us">
-				Want to customize this game for your project?&nbsp;
+				Want to use this game in your project?&nbsp;
 				<a href="https://forms.gle/r1x2L8uTyEZqqgrb7">Contact Us</a>
 			</p>
 		</footer>
