f26558dd4fb2206576e5cad2de80801c484e55a5
gitakileus <akileus0902@gmail.com>
2022-11-30 05:41:21 -0500

    implement vfx on balance

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react'
 import { useGameStore } from 'store/game'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Balance from 'components/Balance'
@@ -5,10 +6,24 @@ import './header.scss'
 
 const Header = () => {
 	const balance = useGameStore((state) => state.balance)
+	const [prev, setPrev] = useState(balance)
+	const ref = useRef<any>(null)
+
+	useEffect(() => {
+		if (balance > prev) {
+			const diff = (balance - prev).toFixed(2)
+			const newElement = document.createElement('span')
+			newElement.innerText = `$ ${diff}`
+			newElement.className = 'diff'
+			ref.current!.appendChild(newElement)
+		}
+		setPrev(balance)
+	}, [balance]) //eslint-disable-line
+
 	return (
 		<header>
-			<img src="/images/logo.png" alt="" className='logo' />
-			<nav>
+			<img src="/images/logo.png" alt="" className="logo" />
+			<nav ref={ref}>
 				<Balance balance={balance} />
 				<WalletMultiButton />
 			</nav>
