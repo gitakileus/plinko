6554c53d5f104e7abc2a5da5891c8e7cd316c69d
gitakileus <akileus0902@gmail.com>
2022-12-15 05:37:12 -0500

    implement animation to contact us & deposit/withdraw modal

--- src/components/ContactModal/index.tsx
+++ src/components/ContactModal/index.tsx
@@ -1,7 +1,19 @@
 import { useEffect } from 'react'
+import Modal from 'react-modal'
 import { ReactComponent as CloseIcon } from 'assets/icons/close-icon.svg'
 import { ReactComponent as PointerHandIcon } from 'assets/icons/pointer-hand.svg'
 import styles from './contact.module.scss'
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
+}
 
 type ContactModalProps = {
 	open: boolean
@@ -20,38 +32,44 @@ const ContactModal = (props: ContactModalProps) => {
 		element.style.overflow = 'hidden'
 	}, [open])
 
-	return open ? (
-		<div className={styles.contact} onClick={onClose}>
-			<div className="gradient-wrapper">
-				<div className="modal-content">
-					<div className="modal-header">
-						<h1>Want to use this game in your project?</h1>
-						<div>
-							<CloseIcon onClick={onClose} />
+	return (
+		<Modal
+			isOpen={open}
+			onRequestClose={onClose}
+			ariaHideApp={false}
+			styles={customStyles}
+			overlayClassName="contact-modal"
+		>
+			<div className={styles.contact} onClick={onClose}>
+				<div className="gradient-wrapper">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1>Want to use this game in your project?</h1>
+							<div>
+								<CloseIcon onClick={onClose} />
+							</div>
 						</div>
-					</div>
-					<div className="modal-body">
-						<img src="/images/shake-hands.png" alt="" />
-						<p>
-							Placeholder text about how this modern
-							<br />
-							<b>Web 3 Casino Solutions</b> Available for Rent
-						</p>
-					</div>
-					<div className="modal-footer">
-						<button
-							onClick={() =>
-								(window.location.href = 'https://forms.gle/r1x2L8uTyEZqqgrb7')
-							}
-						>
-							<PointerHandIcon /> Contact Us
-						</button>
+						<div className="modal-body">
+							<img src="/images/shake-hands.png" alt="" />
+							<p>
+								Placeholder text about how this modern
+								<br />
+								<b>Web 3 Casino Solutions</b> Available for Rent
+							</p>
+						</div>
+						<div className="modal-footer">
+							<button
+								onClick={() =>
+									(window.location.href = 'https://forms.gle/r1x2L8uTyEZqqgrb7')
+								}
+							>
+								<PointerHandIcon /> Contact Us
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	) : (
-		<></>
+		</Modal>
 	)
 }
 
